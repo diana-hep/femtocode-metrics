239b2a0fc624004eefcf32b9cb1234ed69ff86fb
Jim Pivarski <jpivarski@gmail.com>
2017-04-28 07:26:32 -0500

    Ha! Got my factor of 4! Apparently, nested for loops are easier for LLVM or CPU to optimize than a single while loop with dancing indices.

---
+++ explode-kernel/bigrun.py
@@ -0,0 +1,149 @@
+from nested import runme
+
+def testy(xdata, xsize, ydata, ysize, numEvents):
+    outdata = [None] * 1000
+    outsize = [None] * 1000
+    runme(xdata, xsize, ydata, ysize, numEvents, outdata, outsize)
+    outdata[outdata.index(None):] = []
+    outsize[outsize.index(None):] = []
+    return outdata, outsize
+
+xdata = []
+xsize = []
+ydata = []
+ysize = []
+
+xdata += [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+xsize += [3, 2, 2, 2, 3, 2, 2, 2]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 2)
+
+xdata += [7, 8, 9, 10, 11, 12]
+xsize += [3, 0, 0, 0, 3, 2, 2, 2]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 4)
+
+xdata += [7, 8, 9, 10, 11, 12]
+xsize += [0, 3, 2, 2, 2]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 6)
+
+xdata += [1, 2, 3, 4, 5, 6]
+xsize += [3, 2, 2, 2, 3, 0, 0, 0]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 8)
+
+xdata += [1, 2, 3, 4, 5, 6]
+xsize += [3, 2, 2, 2, 0]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 10)
+
+xdata += []
+xsize += [3, 0, 0, 0, 3, 0, 0, 0]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 12)
+
+xdata += []
+xsize += [0, 3, 0, 0, 0]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 14)
+
+xdata += []
+xsize += [0, 0]
+ydata += [1, 2, 3, 4, 5, 6, 7, 8]
+ysize += [4, 4]
+
+testy(xdata, xsize, ydata, ysize, 16)
+
+xdata += [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+xsize += [3, 2, 2, 2, 3, 2, 2, 2]
+ydata += [5, 6, 7, 8]
+ysize += [0, 4]
+
+testy(xdata, xsize, ydata, ysize, 18)
+
+xdata += [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+xsize += [3, 2, 2, 2, 3, 2, 2, 2]
+ydata += [1, 2, 3, 4]
+ysize += [4, 0]
+
+testy(xdata, xsize, ydata, ysize, 20)
+
+xdata += [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+xsize += [3, 2, 2, 2, 3, 2, 2, 2]
+ydata += []
+ysize += [0, 0]
+
+testy(xdata, xsize, ydata, ysize, 22)
+
+xdata += [7, 8, 9, 10, 11, 12]
+xsize += [3, 0, 0, 0, 3, 2, 2, 2]
+ydata += [5, 6, 7, 8]
+ysize += [0, 4]
+
+testy(xdata, xsize, ydata, ysize, 24)
+
+xdata += [7, 8, 9, 10, 11, 12]
+xsize += [0, 3, 2, 2, 2]
+ydata += [5, 6, 7, 8]
+ysize += [0, 4]
+
+testy(xdata, xsize, ydata, ysize, 26)
+
+xdata += []
+xsize += [0, 3, 0, 0, 0]
+ydata += [5, 6, 7, 8]
+ysize += [0, 4]
+
+testy(xdata, xsize, ydata, ysize, 28)
+
+xdata += []
+xsize += [0, 0]
+ydata += [5, 6, 7, 8]
+ysize += [0, 4]
+
+outdata, outsize = testy(xdata, xsize, ydata, ysize, 30)
+
+import numpy
+import numba
+import time
+
+multiplier = 10000
+
+bigxdata = numpy.empty(len(xdata) * multiplier, dtype=numpy.float64)
+bigxsize = numpy.empty(len(xsize) * multiplier, dtype=numpy.uint64)
+bigydata = numpy.empty(len(ydata) * multiplier, dtype=numpy.float64)
+bigysize = numpy.empty(len(ysize) * multiplier, dtype=numpy.uint64)
+bigoutdata = numpy.empty(len(outdata) * multiplier, dtype=numpy.float64)
+bigoutsize = numpy.empty(len(outsize) * multiplier, dtype=numpy.uint64)
+
+for i in range(multiplier):
+    bigxdata[i * len(xdata) : (i + 1) * len(xdata)] = xdata
+    bigxsize[i * len(xsize) : (i + 1) * len(xsize)] = xsize
+    bigydata[i * len(ydata) : (i + 1) * len(ydata)] = ydata
+    bigysize[i * len(ysize) : (i + 1) * len(ysize)] = ysize
+
+compiled = numba.jit(nopython=True)(runme)
+
+compiled(bigxdata, bigxsize, bigydata, bigysize, 30 * multiplier, bigoutdata, bigoutsize)
+
+for i in range(100):
+    startTime = time.time()
+    compiled(bigxdata, bigxsize, bigydata, bigysize, 30 * multiplier, bigoutdata, bigoutsize)
+    endTime = time.time()
+    print endTime - startTime
+
--- explode-kernel/nested.py
+++ explode-kernel/nested.py
@@ -7,6 +7,8 @@ def runme(xdata, xsize, ydata, ysize, numEntries, outdata, outsize):
     sizeLength = 0
 
     for entry in range(numEntries):
+    # entry = 0
+    # while entry < numEntries:
         nest0size = xsize[xsizeindex]
         xsizeindex += 1
 
@@ -18,11 +20,16 @@ def runme(xdata, xsize, ydata, ysize, numEntries, outdata, outsize):
             nest1size = ysize[ysizeindex]
             ysizeindex += 1
             for nest1 in range(nest1size):   # go all levels deep (only one here)
+            # nest1 = 0
+            # while nest1 < nest1size:
                 ydataindex += 1
+                # nest1 += 1
 
         ysizeunwind0 = ysizeindex
         ydataunwind0 = ydataindex
         for nest0 in range(nest0size):
+        # nest0 = 0
+        # while nest0 < nest0size:
             ysizeindex = ysizeunwind0
             ydataindex = ydataunwind0
 
@@ -37,11 +44,16 @@ def runme(xdata, xsize, ydata, ysize, numEntries, outdata, outsize):
                 nest2size = xsize[xsizeindex]
                 xsizeindex += 1
                 for nest2 in range(nest2size):
+                # nest2 = 0
+                # while nest2 < nest2size:
                     xdataindex += 1
+                    # nest2 += 1
 
             xsizeunwind1 = xsizeindex
             xdataunwind1 = xdataindex
             for nest1 in range(nest1size):
+            # nest1 = 0
+            # while nest1 < nest1size:
                 xsizeindex = xsizeunwind1
                 xdataindex = xdataunwind1
 
@@ -52,9 +64,24 @@ def runme(xdata, xsize, ydata, ysize, numEntries, outdata, outsize):
                 sizeLength += 1
 
                 for nest2 in range(nest2size):
+                # nest2 = 0
+                # while nest2 < nest2size:
                     outdata[dataLength] = xdata[xdataindex] * 100 + ydata[ydataindex]
                     dataLength += 1
 
                     xdataindex += 1   # at the end of the nest2size loop because it's an xsize
 
+                    # nest2 += 1
+
                 ydataindex += 1       # at the end of the nest1size loop because it's a ysize
+
+                # nest1 += 1
+
+            # nest0 += 1
+
+        # entry += 1
+
+    # assert xsizeindex == len(xsize)
+    # assert xdataindex == len(xdata)
+    # assert ysizeindex == len(ysize)
+    # assert ydataindex == len(ydata)
